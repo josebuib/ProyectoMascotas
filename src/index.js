9b27e83d3d5db2034e0c8f41beba005cdfe02fbe
josebuib <josebuitragob75@gmail.com>
2024-05-27 16:53:48 -0500

    importacion de dependencias - configuracion de plantillas, rutas y peticiones

--- src/index.js
+++ src/index.js
@@ -1,15 +1,45 @@
 /* -------------------------- Importar dependencias ------------------------- */
 import express from 'express';
+import morgan from 'morgan';
+
+import {join, dirname} from 'path'
+import {fileURLToPath} from 'url'
+import {engine} from 'express-handlebars'
+
+
 /* ----------------------------- Inicializacion ----------------------------- */
 const app = express();
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 /* --------------------------------- setting -------------------------------- */
 app.set('port', process.env.PORT || 4000 );
+
+app.set('views', join(__dirname,'views'));
+
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    layoutsDir: join(app.get('views'),'layouts'), 
+    partialsDir: join(app.get('views'),'partials'),
+    extname: '.hbs'
+}));
+
+app.set('view engine','.hbs');
+
 /* ------------------------------- Middlewares ------------------------------ */
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 /* --------------------------------- routes --------------------------------- */
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
 
 /* ------------------------------ Public files ------------------------------ */
-
+app.use(express.static(join(__dirname, 'public')));
 /* ------------------------------- Run server ------------------------------- */
 app.listen(app.get('port'), ()=>{
     console.log('Server listening on port', app.get('port'));
