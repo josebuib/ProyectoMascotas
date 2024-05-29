2018481542547ff7eccfaf995f5dca2f66476e96
josebuib <josebuitragob75@gmail.com>
2024-05-29 16:11:07 -0500

    Creamos vistas edit las rutas edit y los post del edit

--- src/routes/mascotas.routes.js
+++ src/routes/mascotas.routes.js
@@ -44,5 +44,34 @@ router.get('/deletem/:id', async(req, res)=>{
     }   
 });
 
+router.get('/editm/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const [mascota] = await pool.query('SELECT * FROM mascotas WHERE id = ?', [id]);
+        const mascotaEdit = mascota[0]
+        res.render('mascotas/editm', { mascota: mascotaEdit })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+router.post('/editm/:id', async (req, res)=>{
+    try {
+        const {id} = req.params
+        const {name, genero, age, observacion}  = req.body
+        const editMascota = {
+                                name, 
+                                genero, 
+                                age,
+                                observacion
+                            }
+    
+        await pool.query('UPDATE mascotas SET ? WHERE id = ?', [editMascota, id]);
+        res.redirect('/listm');
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 export default router;
